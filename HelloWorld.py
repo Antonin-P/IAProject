7a14c8986e1ca843651cb45e26836ba5c13eda13
Antonin-P <59565167+Antonin-P@users.noreply.github.com>
2020-01-06 13:50:05 +0100

    first commit
    first Commit

---
+++ HelloWorld.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+phrase = "Hello World!"
+print(phrase)
+
+for i in range(len(phrase)):
+    print(i * " " + phrase[i])
